e5946771e5d0256ae00da46468ae6fe560161122
fam3 <fam3@remotel3.cs.calvin.edu>
2021-12-11 16:07:50 -0500

    help page

--- screens/Instructions.js
+++ screens/Instructions.js
@@ -11,28 +11,26 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 export default function Instructions() {
   return (
     <View style={{ flex: 1, justifyContent: 'right', alignItems: 'left' }}>
-      <Text>Getting Started:</Text>
-      <Text>  -	Start by downloading the app</Text>
-      <Text>  -	Open the app</Text>
+      <Text style = {{fontweight: 'bold'}}>Section 1 :- Creating a patient profile:</Text>
+      <Text>  -	Start by clicking the add a patient button (“+” button) at the bottom right corner</Text>
+      <Text>  -	Fill out all the necessary fields that appear on the popup page.</Text>
+      <Text>  -	Click the “add patient” button. This should create a new profile for the patient at hand</Text>
       <Text> </Text>
-      <Text>Creating a patient profile</Text>
-      <Text>To create a patient profile:</Text>
-      <Text>  -	Start by clicking the add a patient button (add button) at the bottom right corner</Text>
-      <Text>  -	Fill out all the necessary forms on the popup page that appears after you click the add button</Text>
-      <Text>  -	Click done. This should create a new profile for the patient at hand</Text>
-      <Text>NOTE: </Text>
-      <Text>Make sure you fill out all the required forms as they are needed for creating the appropriate patient forms</Text>
+      <Text style = {{fontweight: 'bold'}}>Section 2 :- Accessing visit forms that already exist:</Text>
+      <Text>  -	Start by searching the name of the desired patient</Text>
+      <Text>  -	Proceed by clicking on the patient profile that appears on the home screen </Text>
+      <Text>  - You should see a list of previous patient visits. Select the desired form</Text>
       <Text> </Text>
-      <Text>Finding and editing/updating a patient that already exists</Text>
-      <Text>To find a patient that already exists:</Text>
-      <Text>  -	Click on the search bar and proceed by entering the full name of the patient</Text>
-      <Text>  -	Click on the first patient profile that appear on the </Text>
-      <Text>  -	After being redirected to the next page, click on the edit button to edit a patient</Text>
-      <Text>  -	Click on the delete button to delete a patient</Text>
+      <Text style = {{fontweight: 'bold'}}>Section 3:- Finding and editing/updating a patient visit that already exists: </Text>
+      <Text>  -	Click on the search bar and proceed by entering the full name of the patient.</Text>
+      <Text>  -	Click on the first patient profile that appear on the home screen</Text>
+      <Text>  -	After being redirected to the next page, click on visit form you want to edit</Text>
+      <Text>  -	Click on the delete button to delete or the edit button to edit the visit form</Text>
       <Text> </Text>
-      <Text>Syncing the patient profile database</Text>
-      <Text>To sync your patient profile:</Text>
+      <Text style = {{fontweight: 'bold'}}>Section 4:- Syncing the patient  database: </Text>
       <Text>  -	Click on the sync button at the top right corner</Text>
+      <Text>  - This should synchronize your database accross all your devices</Text>
+
     </View>
   );
 }
