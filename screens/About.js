302b7fc2e33467eb00fbced9e0a50a713210b63f
fam3 <fam3@remotel2.cs.calvin.edu>
2021-12-11 13:24:49 -0500

    instructions update

--- screens/About.js
+++ screens/About.js
@@ -10,15 +10,18 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 export default function About() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Opus is a mobile software that will record medical information input
-        by EMA’s medical staff on their phones for ease of collective access. Opus
-        allows doctors an offline input and storage of medical data which can then
-        uploaded to a server and updated locally once the user reaches an area with
-        an internet connection. This added functionality is needed to allow the
-        management of patient medical data when wifi and cellular reception are
-        not available, a necessity for EMA as they send their medical staff deeper
-        into the Myanmar jungles. After being completed, opus will hopefully easie
-        the hardship that doctors in remote areas (with out access to internet connection) face.   </Text>
+      <Text>WELCOME TO OPUS!</Text>
+      <Text> </Text>
+      <Text>Opus is a mobile software that will record medical information input</Text>
+      <Text>by EMA’s medical staff on their phones for ease of collective access. Opus</Text>
+      <Text>allows doctors an offline input and storage of medical data which can then</Text>
+      <Text> allows doctors an offline input and storage of medical data which can then</Text>
+      <Text>uploaded to a server and updated locally once the user reaches an area with</Text>
+      <Text> an internet connection. This added functionality is needed to allow the</Text>
+      <Text>management of patient medical data when wifi and cellular reception are</Text>
+      <Text>not available, a necessity for EMA as they send their medical staff deeper</Text>
+      <Text>into the Myanmar jungles. After being completed, opus will hopefully easie</Text>
+      <Text> the hardship that doctors in remote areas (with out access to internet connection) face.   </Text>
     </View>
   );
 }
--- screens/Instructions.js
+++ screens/Instructions.js
@@ -10,8 +10,29 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
  */
 export default function Instructions() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Instructions</Text>
+    <View style={{ flex: 1, justifyContent: 'right', alignItems: 'left' }}>
+      <Text>Getting Started:</Text>
+      <Text>  -	Start by downloading the app</Text>
+      <Text>  -	Open the app</Text>
+      <Text> </Text>
+      <Text>Creating a patient profile</Text>
+      <Text>To create a patient profile:</Text>
+      <Text>  -	Start by clicking the add a patient button (add button) at the bottom right corner</Text>
+      <Text>  -	Fill out all the necessary forms on the popup page that appears after you click the add button</Text>
+      <Text>  -	Click done. This should create a new profile for the patient at hand</Text>
+      <Text>NOTE: </Text>
+      <Text>Make sure you fill out all the required forms as they are needed for creating the appropriate patient forms</Text>
+      <Text> </Text>
+      <Text>Finding and editing/updating a patient that already exists</Text>
+      <Text>To find a patient that already exists:</Text>
+      <Text>  -	Click on the search bar and proceed by entering the full name of the patient</Text>
+      <Text>  -	Click on the first patient profile that appear on the </Text>
+      <Text>  -	After being redirected to the next page, click on the edit button to edit a patient</Text>
+      <Text>  -	Click on the delete button to delete a patient</Text>
+      <Text> </Text>
+      <Text>Syncing the patient profile database</Text>
+      <Text>To sync your patient profile:</Text>
+      <Text>  -	Click on the sync button at the top right corner</Text>
     </View>
   );
 }
